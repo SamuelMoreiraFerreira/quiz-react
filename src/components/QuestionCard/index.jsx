63c85a75c333b2bde57a58d29e15d7e6efe4cc93
Pablo Moralez <pablo.moralez@aluno.senai.br>
2025-09-29 11:27:52 -0300

    FIX Timer

--- src/components/QuestionCard/index.jsx
+++ src/components/QuestionCard/index.jsx
@@ -1,25 +1,45 @@
 import { useEffect, useState } from 'react';
 import styles from './card.module.css';
 
-const optionsMark = ['A', 'B', 'C', 'D', 'E'];
+const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
 export default function QuestionCard ({ question, options, callbackAnswer })
 {
 
     //#region Timer
 
-    const startedTimer = Date.now();
-    const [sla, setTimer] = useState(Date.now());
+    const maxTime = 0.25 * 60 * 1000;
+    const [endTime, _] = useState(Date.now() + maxTime);
+
+    const [timer, setTimer] = useState(Date.now());
+    const remainingTime = new Date(endTime - timer);
 
     useEffect (
 
         function ()
         {
-            const timer = setInterval(() => setTimer(t => Date.now()), 1000);
-            return () => clearInterval(timer);
-        }
+            const intervalTimer = setInterval(
+                
+                function ()
+                {
+                    setTimer(Date.now());
 
-    )
+                    if (timer > endTime)
+                    {
+                        callbackAnswer(0);
+                    }
+                }, 
+                
+                100
+            
+            );
+
+            return () => clearInterval(intervalTimer);
+        },
+
+        [endTime]
+
+    );
 
     //#endregion
 
@@ -28,7 +48,17 @@ export default function QuestionCard ({ question, options, callbackAnswer })
         <article className={styles.card}>
 
             <header className={styles.cardHeader}>
-                <label>{Date(sla)}</label>
+
+                <span class="material-symbols-outlined">
+                    timer
+                </span>
+
+                <label>
+                    {remainingTime.getMinutes().toString().padStart(2, '0')}:
+                    {remainingTime.getSeconds().toString().padStart(2, '0')}:
+                    {remainingTime.getMilliseconds().toString().padStart(2, '0')}
+                </label>
+
             </header>
 
             <h2 className={styles.cardQuestion}>{question}</h2>
@@ -45,7 +75,7 @@ export default function QuestionCard ({ question, options, callbackAnswer })
                                     callbackAnswer(index)
                                 }}
                             >
-                                <label>{optionsMark[index]}</label>
+                                <label>{alphabet[index]}</label>
                                 <label>{option}</label>
                             </button>
                         </li>
